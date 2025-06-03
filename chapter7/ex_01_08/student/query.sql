db26b08c981020ebbd1fd848f0f96c24b6cb8fbe
jcamdev247 <jcam.dev247@gmail.com>
2025-06-03 02:05:23 +0000

    Testing Tue Jun 03 2025 02:05:23 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_01_08/student/query.sql
+++ chapter7/ex_01_08/student/query.sql
@@ -1,1 +1,9 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
+SELECT
+    EMP_NUM, EMP_LNAME, EMP_FNAME, EMP_INITIAL
+FROM 
+    EMPLOYEE
+WHERE 
+    EMP_LNAME = 'Smith'
+OR  EMP_LNAME = 'Smithfield';
+
