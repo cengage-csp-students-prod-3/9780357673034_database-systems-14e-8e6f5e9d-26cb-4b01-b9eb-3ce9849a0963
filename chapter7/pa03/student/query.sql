b7eba32e639810296d02de0b3116add27d003e3a
jcamdev247 <jcam.dev247@gmail.com>
2025-06-04 23:29:47 +0000

    Testing Wed Jun 04 2025 23:29:47 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa03/student/query.sql
+++ chapter7/pa03/student/query.sql
@@ -1,15 +1,8 @@
--- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
+-- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.()
 SELECT
-DRIVER_ID, DRIVER_FIRST_NAME, DRIVER_LAST_NAME
+CAR_ID, ROUND(AVG(ROUND(TRAVEL_PRICE * TRAVEL_DISCOUNT,2)),2) AS DISCOUNT_AMOUNT
 FROM
-DRIVERS
-WHERE
-DRIVERS.DRIVER_ID = 
-(SELECT 
-D.DRIVER_ID
-FROM
-DRIVERS D
-WHERE 
-D.DRIVER_ID != ALL (SELECT DISTINCT T.DRIVER_ID FROM TRAVELS T)
-);
+TRAVELS 
+WHERE TRAVEL_DISCOUNT > 0
+GROUP BY CAR_ID;
 
