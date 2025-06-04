26a8db9c17cea873e99c210f0e771448549eb7bc
jcamdev247 <jcam.dev247@gmail.com>
2025-06-04 01:52:03 +0000

    Testing Wed Jun 04 2025 01:52:03 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa01/student/query.sql
+++ chapter7/pa01/student/query.sql
@@ -1,13 +1,5 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-SELECT
-*
-FROM
-DRIVERS;
 
-SELECT 
-DATE_FORMAT(DRIVER_START_DATE, "%Y-%m-%d") AS DRIVER_START_DATE
-FROM 
-DRIVERS;
 
 SELECT
  DRIVER_ID, DRIVER_RATING
