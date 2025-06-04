d7ab3575e177b6599ad1ce5107e58736465f4796
jcamdev247 <jcam.dev247@gmail.com>
2025-06-04 18:22:01 +0000

    Testing Wed Jun 04 2025 18:22:01 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa03/student/query.sql
+++ chapter7/pa03/student/query.sql
@@ -1,8 +1,8 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
 SELECT
-*
-FROM TRAVELS T
-WHERE T.DRIVER_ID IN
+D.DRIVER_ID, D.DRIVER_FIRST_NAME, D.DRIVER_LAST_NAME
+FROM DRIVERS D
+WHERE DRIVER IN
 (SELECT
 D.DRIVER_ID
 FROM
