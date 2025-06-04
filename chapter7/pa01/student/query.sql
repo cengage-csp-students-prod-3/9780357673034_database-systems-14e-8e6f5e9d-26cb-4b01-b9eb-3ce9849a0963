751128eaa369d018caad35abc4909a504e751104
jcamdev247 <jcam.dev247@gmail.com>
2025-06-04 01:57:18 +0000

    Testing Wed Jun 04 2025 01:57:18 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa01/student/query.sql
+++ chapter7/pa01/student/query.sql
@@ -1,11 +1,7 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-
-
-SELECT
- DRIVER_ID, DRIVER_RATING
- FROM
- DRIVERS
- WHERE DRIVER_RATING > 4
- ORDER BY DRIVER_RATING DESC;
-
-   
+ SELECT 
+ U.USER_ID, TIMEDIFF(T.TRAVEL_END_TIME, T.TRAVEL_START_TIME) AS 'TRAVEL_TIME '
+ FROM 
+ TRAVELS T, USERS U
+ WHERE
+ T.USER_ID = U.USER_ID;
