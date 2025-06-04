1307efd3f2f33ccb8b6098e9b69801d4bcd439c8
jcamdev247 <jcam.dev247@gmail.com>
2025-06-04 02:24:04 +0000

    Testing Wed Jun 04 2025 02:24:04 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa01/student/query.sql
+++
@@ -1,5 +0,0 @@
--- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-SELECT
-DATE_FORMAT(T.TRAVEL_END_TIME, "%M") AS LAST_TRAVEL_MONTH, DATE_ADD(LAST_TRAVEL_MONTH, INTERVAL 1 WEEK)
-FROM
-TRAVELS T;
