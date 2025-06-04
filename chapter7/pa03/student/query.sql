06f6acf68679020c2171deead14d809ae965ec00
jcamdev247 <jcam.dev247@gmail.com>
2025-06-04 23:33:04 +0000

    Testing Wed Jun 04 2025 23:33:04 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa03/student/query.sql
+++ chapter7/pa03/student/query.sql
@@ -1,8 +1,1 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.()
-SELECT
-CAR_ID, ROUND(AVG(ROUND(TRAVEL_PRICE * TRAVEL_DISCOUNT,2)),2) AS DISCOUNT_AMOUNT
-FROM
-TRAVELS 
-WHERE TRAVEL_DISCOUNT > 0
-GROUP BY CAR_ID;
-
