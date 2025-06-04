53dcb2f6f877aba54885bf0c9d18090e18a8aea3
jcamdev247 <jcam.dev247@gmail.com>
2025-06-04 01:14:12 +0000

    Testing Wed Jun 04 2025 01:14:12 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa01/student/query.sql
+++ chapter7/pa01/student/query.sql
@@ -1,8 +1,5 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-SELECT
-    *
-FROM
-    DRIVERS;
+
 
 SELECT 
 DRIVER_START_DATE
