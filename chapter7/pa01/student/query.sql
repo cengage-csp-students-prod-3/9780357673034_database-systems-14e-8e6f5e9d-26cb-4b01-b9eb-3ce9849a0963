109a627cf142415bc265f4b83ab4603680f2046b
jcamdev247 <jcam.dev247@gmail.com>
2025-06-04 02:00:17 +0000

    Testing Wed Jun 04 2025 02:00:17 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa01/student/query.sql
+++ chapter7/pa01/student/query.sql
@@ -1,6 +1,6 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
  SELECT 
- U.USER_ID, TIMEDIFF(T.TRAVEL_END_TIME, T.TRAVEL_START_TIME) AS 'TRAVEL_TIME '
+ U.USER_ID, TIMEDIFF(T.TRAVEL_END_TIME, T.TRAVEL_START_TIME) AS TRAVEL_TIME
  FROM 
  TRAVELS T, USERS U
  WHERE
