03edc8163cb67c80acb8bcd4601728a3ddf3fa73
jcamdev247 <jcam.dev247@gmail.com>
2025-06-05 02:16:41 +0000

    Testing Thu Jun 05 2025 02:16:41 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa03/student/query.sql
+++ chapter7/pa03/student/query.sql
@@ -1,27 +1,7 @@
-SELECT
-    TUSAGES.CAR_ID,
-    TUSAGES.Usages
+SELECT 
+DRIVER_FIRST_NAME AS FIRST_NAME, DRIVER_LAST_NAME AS LAST_NAME
+FROM DRIVERS 
+UNION
+SELECT USER_FIRST_NAME AS FIRST_NAME, USER_LAST_NAME
 FROM
-    ( -- Level 1: Calculate the usage count for each CAR_ID
-        SELECT
-            CAR_ID,
-            COUNT(TRAVEL_ID) AS Usages
-        FROM
-            TRAVELS
-        GROUP BY
-            CAR_ID
-    ) AS TUSAGES
-WHERE
-    TUSAGES.Usages > ( -- Level 2: Calculate the average of all individual car usage counts
-        SELECT
-            AVG(CarUsageCounts.Usages)
-        FROM
-            ( -- This subquery gets the individual usage counts for averaging
-                SELECT
-                    COUNT(TRAVEL_ID) AS Usages
-                FROM
-                    TRAVELS
-                GROUP BY
-                    CAR_ID
-            ) AS CarUsageCounts
-    );
+USERS;
--- chapter7/pa03/student/sqlRepo.sql
+++ chapter7/pa03/student/sqlRepo.sql
@@ -53,3 +53,31 @@ GROUP BY T.CAR_ID;
 SELECT AVG(TIMEDIFF(TRAVEL_END_TIME,TRAVEL_START_TIME))
 FROM
 TRAVELS T;
+
+SELECT
+    TUSAGES.CAR_ID,
+    TUSAGES.Usages
+FROM
+    ( -- Level 1: Calculate the usage count for each CAR_ID
+        SELECT
+            CAR_ID,
+            COUNT(TRAVEL_ID) AS Usages
+        FROM
+            TRAVELS
+        GROUP BY
+            CAR_ID
+    ) AS TUSAGES
+WHERE
+    TUSAGES.Usages > ( -- Level 2: Calculate the average of all individual car usage counts
+        SELECT
+            AVG(CarUsageCounts.Usages)
+        FROM
+            ( -- This subquery gets the individual usage counts for averaging
+                SELECT
+                    COUNT(TRAVEL_ID) AS Usages
+                FROM
+                    TRAVELS
+                GROUP BY
+                    CAR_ID
+            ) AS CarUsageCounts
+    );
