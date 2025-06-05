9b53da8f23d3b851387544c584976c3aa2d89f26
jcamdev247 <jcam.dev247@gmail.com>
2025-06-05 02:30:13 +0000

    Testing Thu Jun 05 2025 02:30:13 GMT+0000 (Coordinated Universal Time)

--- chapter7/pa04/student/query.sql
+++ chapter7/pa04/student/query.sql
@@ -1,1 +1,6 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
+SELECT
+CAR_ID, COUNT(CAR_ID)
+FROM
+TRAVELS
+GROUP BY CAR_ID;
