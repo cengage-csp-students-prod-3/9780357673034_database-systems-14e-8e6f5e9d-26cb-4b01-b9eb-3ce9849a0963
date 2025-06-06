f8d4486fb58ee1655a6a489b740718aa7574b7f9
jcamdev247 <jcam.dev247@gmail.com>
2025-06-06 13:35:36 +0000

    Testing Fri Jun 06 2025 13:35:36 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,6 +1,18 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
 SELECT 
-COUNT(*)
+CUS_CODE,INV_NUMBER,INV_DATE,P_DESCRIPT,LINE_UNITS,LINE_PRICE
 FROM
-CUSTOMER
-WHERE CUS_BALANCE > 500;
+CUSTOMER C
+JOIN
+INVOICE I
+ON
+C.CUS_CODE = I.CUS_CODE
+JOIN
+Ch07_SaleCoLINE L
+ON
+L.INV_NUMBER = L.INV_NUMBER
+JOIN
+PRODUCT P
+ON
+L.P_CODE = P.P_CODE
+
