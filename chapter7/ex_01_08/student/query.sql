9a25d87e6b036fee0654317d426f9ca7f87c046b
jcamdev247 <jcam.dev247@gmail.com>
2025-06-06 01:04:21 +0000

    Testing Fri Jun 06 2025 01:04:21 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_01_08/student/query.sql
+++ chapter7/ex_01_08/student/query.sql
@@ -1,16 +1,6 @@
--- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-SELECT
-P.PROJ_NAME, P.PROJ_VALUE,P.PROJ_BALANCE, E.EMP_LNAME, E.EMP_FNAME, E.EMP_INITIAL, J.JOB_CODE, J.JOB_DESCRIPTION, J.JOB_CHG_HOUR 
+SELECT DISTINCT
+PROJ_NUM
 FROM 
-      JOB J
-JOIN 
-    EMPLOYEE E 
-ON E.JOB_CODE = J.JOB_CODE
-JOIN
- PROJECT P
-ON 
-P.EMP_NUM = E.EMP_NUM
-ORDER BY E.EMP_LNAME;
-
-
-
+PROJECT
+ORDER BY 
+PROJ_NUM;
