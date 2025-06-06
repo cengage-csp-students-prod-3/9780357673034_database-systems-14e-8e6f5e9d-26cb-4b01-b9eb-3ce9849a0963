1734c8393985cf18dfa1842e0d68c856f22722cb
jcamdev247 <jcam.dev247@gmail.com>
2025-06-06 01:03:01 +0000

    Testing Fri Jun 06 2025 01:03:01 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_01_08/student/repo.sql
+++ chapter7/ex_01_08/student/repo.sql
@@ -1,4 +1,3 @@
--- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
 SELECT
 P.PROJ_NAME, P.PROJ_VALUE,P.PROJ_BALANCE, E.EMP_LNAME, E.EMP_FNAME, E.EMP_INITIAL, J.JOB_CODE, J.JOB_DESCRIPTION, J.JOB_CHG_HOUR 
 FROM 
@@ -10,7 +9,7 @@ JOIN
  PROJECT P
 ON 
 P.EMP_NUM = E.EMP_NUM
-ORDER BY E.EMP_LNAME;
+ORDER BY P.PROJ_VALUE;
 
 
 
