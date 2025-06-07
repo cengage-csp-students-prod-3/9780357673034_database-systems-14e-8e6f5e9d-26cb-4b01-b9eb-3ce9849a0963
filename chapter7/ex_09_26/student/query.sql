7bb5eb01d100bd10075e6113e2a273ba2d708fb6
jcamdev247 <jcam.dev247@gmail.com>
2025-06-07 02:39:25 +0000

    Testing Sat Jun 07 2025 02:39:25 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,25 +1,23 @@
 USE Ch07_SaleCo;
 
-SELECT
-    COUNT(DISTINCT I.INV_NUMBER) AS "Total Invoices",
-    ROUND(SUM(L.LINE_UNITS * L.LINE_PRICE),2) AS "Total Sales",
-    MIN(CustomerPurchases.Total_Purchases) AS "Minimum Customer Purchases",
-    MAX(CustomerPurchases.Total_Purchases) AS "Largest Customer Purchases",
-    ROUND(AVG(CustomerPurchases.Total_Purchases),2) AS "Average Customer Purchases"
-FROM
-    INVOICE AS I
-JOIN
-    Ch07_SaleCoLINE AS L ON I.INV_NUMBER = L.INV_NUMBER
-JOIN
-    (SELECT
+WITH CustomerTotalSales AS (
+    SELECT
         C.CUS_CODE,
-        ROUND(SUM(L2.LINE_UNITS * L2.LINE_PRICE),2) AS Total_Purchases
+        SUM(L.LINE_UNITS * L.LINE_PRICE) AS Total_Purchases
     FROM
         CUSTOMER AS C
     JOIN
-        INVOICE AS I2 ON C.CUS_CODE = I2.CUS_CODE
+        INVOICE AS I ON C.CUS_CODE = I.CUS_CODE
     JOIN
-        Ch07_SaleCoLINE AS L2 ON I2.INV_NUMBER = L2.INV_NUMBER
+        Ch07_SaleCoLINE AS L ON I.INV_NUMBER = L.INV_NUMBER
     GROUP BY
         C.CUS_CODE
-    ) AS CustomerPurchases ON I.CUS_CODE = CustomerPurchases.CUS_CODE;
+)
+SELECT
+    (SELECT COUNT(DISTINCT INV_NUMBER) FROM INVOICE) AS "Total Invoices", -- Counts all unique invoices
+    (SELECT ROUND(SUM(LINE_UNITS * LINE_PRICE)) FROM Ch07_SaleCoLINE) AS "Total Sales", -- Sums all line item totals
+    ROUND(MIN(CTS.Total_Purchases),2) AS "Minimum Customer Purchases",
+    ROUND(MAX(CTS.Total_Purchases),2) AS "Largest Customer Purchases",
+    ROUND(AVG(CTS.Total_Purchases), 2) AS "Average Customer Purchases" -- Correct AVG of per-customer totals
+FROM
+    CustomerTotalSales AS CTS;
