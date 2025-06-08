fd332ad5a6ac5f494f0f9710d0b748f96993f32a
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 14:24:43 +0000

    Testing Sun Jun 08 2025 14:24:43 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,16 +1,10 @@
 SELECT
-    A.AU_ID,       -- Author ID
-    A.AU_FNAME,    -- Author First Name
-    A.AU_LNAME     -- Author Last Name
+    BOOK_NUM,
+    BOOK_TITLE,
+    BOOK_SUBJECT,
+    ROUND(AVG(BOOK_COST) OVER (PARTITION BY BOOK_SUBJECT), 2) AS AVGCOST,
+    ROUND(BOOK_COST - AVG(BOOK_COST) OVER (PARTITION BY BOOK_SUBJECT), 2) AS Difference
 FROM
-    AUTHOR AS A
-LEFT JOIN
-    -- Join with WRITES and then BOOK to find books written by the author, specifically those with 'Programming' subject
-    WRITES AS W ON A.AU_ID = W.AU_ID
-LEFT JOIN
-    BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM AND B.BOOK_SUBJECT = 'Programming'
-WHERE
-    -- Filter for authors who had no matching 'Programming' book
-    B.BOOK_NUM IS NULL
+    BOOK
 ORDER BY
-    A.AU_LNAME ASC; -- Sort by author last name in ascending order
+    BOOK_TITLE ASC;
