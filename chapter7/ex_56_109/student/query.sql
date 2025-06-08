028ea28c5b18c6e25768f94972d07a4aec9bd3f1
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 12:17:04 +0000

    Testing Sun Jun 08 2025 12:17:04 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,13 +1,8 @@
 SELECT
-    PAT_ID AS PATRON,
-    BOOK_NUM AS BOOK,
-    (CHECK_IN_DATE - CHECK_OUT_DATE) AS `Days Kept` -- This syntax is common in PostgreSQL/Oracle for DATE type subtraction
-    -- If your environment is MySQL and DATEDIFF was giving 0, this might be a specific test environment issue.
-    -- For MySQL, the syntax is DATEDIFF(CHECK_IN_DATE, CHECK_OUT_DATE)
-    -- For SQL Server, the syntax is DATEDIFF(day, CHECK_OUT_DATE, CHECK_IN_DATE)
+    BOOK_NUM,
+    CONCAT(BOOK_TITLE, ' (', BOOK_YEAR, ')') AS `BOOK`, -- Concatenates title and year in desired format
+    BOOK_SUBJECT
 FROM
-    CHECKOUT
+    BOOK
 ORDER BY
-    `Days Kept` DESC, -- Sort by days kept in descending order (NULLs will be at the end or beginning depending on DB)
-    PATRON ASC,       -- Then by patron ID in ascending order
-    BOOK ASC;         -- Then by book number in ascending order
+    BOOK_NUM ASC;
