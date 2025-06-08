8e4a2fa35669b79079f56ce4df1393c1df109a89
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 13:13:08 +0000

    Testing Sun Jun 08 2025 13:13:08 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,20 +1,14 @@
 SELECT
-    B.BOOK_NUM,      -- Book Number
-    B.BOOK_TITLE,    -- Book Title
-    A.AU_LNAME,      -- Author Last Name
-    A.AU_FNAME,      -- Author First Name
-    P.PAT_ID,        -- Patron ID
-    P.PAT_LNAME,     -- Patron Last Name
-    P.PAT_TYPE       -- Patron Type
+    B.BOOK_NUM,
+    B.BOOK_TITLE,
+    COUNT(C.CHECK_NUM) AS TIMES_CHECKED_OUT -- Counts non-null checkouts, resulting in 0 for books never checked out
 FROM
     BOOK AS B
-JOIN
-    PATRON AS P ON B.PAT_ID = P.PAT_ID -- Join BOOK with PATRON for current checkouts
-JOIN
-    WRITES AS W ON B.BOOK_NUM = W.BOOK_NUM -- Join BOOK with WRITES to link to authors
-JOIN
-    AUTHOR AS A ON W.AU_ID = A.AU_ID -- Join WRITES with AUTHOR for author details
-WHERE
-    B.PAT_ID IS NOT NULL -- Ensures only currently checked out books are included
+LEFT JOIN
+    CHECKOUT AS C ON B.BOOK_NUM = C.BOOK_NUM -- Includes all books, even if they have no matching checkouts
+GROUP BY
+    B.BOOK_NUM,
+    B.BOOK_TITLE
 ORDER BY
-    B.BOOK_TITLE ASC;    -- Sort results by book title
+    TIMES_CHECKED_OUT DESC, -- Sort by the number of times checked out in descending order
+    B.BOOK_TITLE ASC;     -- Then by book title in ascending order
