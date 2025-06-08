8e511941275244101e4bee0fd3496cfea141fd6a
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 00:25:01 +0000

    Testing Sun Jun 08 2025 00:25:01 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,23 +1,16 @@
 USE Ch07_SaleCo;
 
-WITH CustomerTotalSales AS (
+WITH BALANCES AS (
     SELECT
-        C.CUS_CODE,
-        SUM(L.LINE_UNITS * L.LINE_PRICE) AS Total_Purchases
+        ROUND(MIN(C.CUS_BALANCE),2) AS `MINIMUM BALANCES`,
+        ROUND(MAX(C.CUS_BALANCE),2) AS `MAXIMUM BALANCES`,
+        ROUND(AVG(C.CUS_BALANCE),2) AS `AVERAGE BALANCES`
     FROM
         CUSTOMER AS C
-    JOIN
-        INVOICE AS I ON C.CUS_CODE = I.CUS_CODE
-    JOIN
-        Ch07_SaleCoLINE AS L ON I.INV_NUMBER = L.INV_NUMBER
-    GROUP BY
-        C.CUS_CODE
 )
 SELECT
-    (SELECT COUNT(DISTINCT INV_NUMBER) FROM INVOICE) AS "Total Invoices", -- Counts all unique invoices
-    (SELECT ROUND(SUM(LINE_UNITS * LINE_PRICE),2) FROM Ch07_SaleCoLINE) AS "Total Sales", -- Sums all line item totals
-    ROUND(MIN(CTS.Total_Purchases),2) AS "Minimum Customer Purchases",
-    ROUND(MAX(CTS.Total_Purchases),2) AS "Largest Customer Purchases",
-    ROUND(AVG(CTS.Total_Purchases), 2) AS "Average Customer Purchases" -- Correct AVG of per-customer totals
+    B.`MINIMUM BALANCES`,
+    B.`MAXIMUM BALANCES`,
+    B. `AVERAGE BALANCES`
 FROM
-    CustomerTotalSales AS CTS;
+   BALANCES AS B;
