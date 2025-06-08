c4e79dddbaff43b3c7a0df16123a7c0c940628ed
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 12:55:38 +0000

    Testing Sun Jun 08 2025 12:55:38 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,10 +1,14 @@
 SELECT
-    PAT_ID,
-    CONCAT(PAT_FNAME, ' ', PAT_LNAME) AS `Patron Full Name`, -- Concatenates first and last names
-    PAT_TYPE
+    A.AU_LNAME,        -- Author's last name
+    A.AU_FNAME,        -- Author's first name
+    B.BOOK_TITLE,      -- Book title
+    B.BOOK_COST AS `BOOK_COST` -- Book cost, aliased as 'Replacement Cost'
 FROM
-    PATRON
+    AUTHOR AS A
+JOIN
+    WRITES AS W ON A.AU_ID = W.AU_ID -- Link authors to books via the WRITES table
+JOIN
+    BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM -- Link WRITES to BOOK table
 ORDER BY
-    LOWER(PAT_TYPE) ASC, -- Sorts by patron type case-insensitively
-    LOWER(PAT_LNAME) ASC, -- Then by last name case-insensitively
-    LOWER(PAT_FNAME) ASC; -- Then by first name case-insensitively
+    B.BOOK_NUM ASC,    -- Sort by book number
+    A.AU_ID ASC;       -- Then by author ID
