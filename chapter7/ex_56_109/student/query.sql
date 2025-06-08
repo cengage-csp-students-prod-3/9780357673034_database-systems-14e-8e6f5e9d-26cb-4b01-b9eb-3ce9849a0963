f413f37f80d43199fcc4644418767f317eb92421
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 11:48:02 +0000

    Testing Sun Jun 08 2025 11:48:02 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,11 +1,4 @@
 SELECT
-    A.AU_ID,
-    COUNT(W.BOOK_NUM) AS  `BOOKS WRITTEN` -- Count the books associated with each author
+    SUM(BOOK_COST) AS `LIBRARY VALUE`
 FROM
-    AUTHOR AS A
-JOIN
-    WRITES AS W ON A.AU_ID = W.AU_ID -- Join AUTHOR with WRITES to link authors to books
-GROUP BY
-    A.AU_ID
-ORDER BY
-    `BOOKS WRITTEN` DESC
+    BOOK;
