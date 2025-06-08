149e06705cfc97480fcffad586a442c8ca3d852d
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 14:24:22 +0000

    Testing Sun Jun 08 2025 14:24:22 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,15 +1,16 @@
 SELECT
-    BOOK_NUM,
-    BOOK_TITLE,
-    BOOK_COST
+    A.AU_ID,       -- Author ID
+    A.AU_FNAME,    -- Author First Name
+    A.AU_LNAME     -- Author Last Name
 FROM
-BOOK
-WHERE (SELECT
-MIN(BOOK_COST)
-FROM
-BOOK) = BOOK_COST
-GROUP BY
-    BOOK_NUM,
-    BOOK_TITLE
+    AUTHOR AS A
+LEFT JOIN
+    -- Join with WRITES and then BOOK to find books written by the author, specifically those with 'Programming' subject
+    WRITES AS W ON A.AU_ID = W.AU_ID
+LEFT JOIN
+    BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM AND B.BOOK_SUBJECT = 'Programming'
+WHERE
+    -- Filter for authors who had no matching 'Programming' book
+    B.BOOK_NUM IS NULL
 ORDER BY
-BOOK_NUM
+    A.AU_LNAME ASC; -- Sort by author last name in ascending order
