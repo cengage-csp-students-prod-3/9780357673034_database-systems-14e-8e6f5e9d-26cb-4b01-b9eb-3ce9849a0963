de69d544ef00e19432f3d832a91552538691bf92
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 11:05:40 +0000

    Testing Sun Jun 08 2025 11:05:40 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,10 +1,10 @@
 SELECT
     AU_ID,
     AU_FNAME,
-    AU_LNAME,
-    AU_BIRTHYEAR
+    AU_LNAME
 FROM
     AUTHOR
+WHERE
+    AU_BIRTHYEAR IS NOT NULL
 ORDER BY
-    AU_BIRTHYEAR DESC, -- Sorts by year of birth in descending order
-    AU_LNAME ASC;      -- Then sorts by last name in ascending order for authors with the same birth year
+    AU_ID;
