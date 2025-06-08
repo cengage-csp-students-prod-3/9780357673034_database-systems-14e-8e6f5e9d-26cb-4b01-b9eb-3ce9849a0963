ff2fcc843c10de82d2bd53c30d5e0ba5857791f2
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 14:45:39 +0000

    Testing Sun Jun 08 2025 14:45:39 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,18 +1,17 @@
 SELECT
-    A.AU_ID,       -- Author ID
-    A.AU_FNAME,    -- Author First Name
-    A.AU_LNAME     -- Author Last Name
+    B.BOOK_NUM,         -- Book Number
+    B.BOOK_TITLE,       -- Book Title
+    B.BOOK_SUBJECT,     -- Book Subject
+    A.AU_LNAME,         -- Author Last Name
+    COUNT(W.BOOK_NUM) OVER (PARTITION BY A.AU_ID) AS NumBooksByAuthor -- Counts all books written by each author
 FROM
-    AUTHOR AS A
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM
-        WRITES AS W
-    JOIN
-        BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM
-    WHERE
-        W.AU_ID = A.AU_ID          -- Link to the outer query's author
-        AND B.BOOK_SUBJECT = 'Programming' -- Filter for 'Programming' subject
-)
+    BOOK AS B
+JOIN
+    WRITES AS W ON B.BOOK_NUM = W.BOOK_NUM -- Link books to the authors who wrote them
+JOIN
+    AUTHOR AS A ON W.AU_ID = A.AU_ID       -- Link authors to their details
+WHERE
+    B.BOOK_SUBJECT = 'Cloud' -- Filter for books specifically in the 'Cloud' subject
 ORDER BY
-    A.AU_LNAME ASC; -- Sort by author last name in ascending order
+    B.BOOK_TITLE ASC,   -- Sort by book title in ascending order
+    A.AU_LNAME ASC;     -- Then by author last name in ascending order
