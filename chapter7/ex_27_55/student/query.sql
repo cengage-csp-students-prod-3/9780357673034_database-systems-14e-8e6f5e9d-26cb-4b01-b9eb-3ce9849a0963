3e27a0834792d63bf3cadcb58b3e8ccfcf994648
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 06:19:16 +0000

    Testing Sun Jun 08 2025 06:19:16 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_27_55/student/query.sql
+++ chapter7/ex_27_55/student/query.sql
@@ -1,15 +1,34 @@
 SELECT
-    B.BRAND_ID,
-    B.BRAND_NAME,
-    B.BRAND_TYPE,
-    ROUND(AVG(P.PROD_PRICE), 2) AS AVGPRICE
+  EM.EMP_FNAME AS ManagerFirstName,
+  EM.EMP_LNAME AS ManagerLastName,
+  D.DEPT_NAME,
+  D.DEPT_PHONE,
+  ES.EMP_FNAME AS EmployeeFirstName,
+  ES.EMP_LNAME AS EmployeeLastName,
+  C.CUST_FNAME AS CustomerFirstName,
+  C.CUST_LNAME AS CustomerLastName,
+  I.INV_DATE,
+  I.INV_TOTAL
 FROM
-    LGBRAND B
+  LGCUSTOMER AS C
 JOIN
-    LGPRODUCT P ON B.BRAND_ID = P.BRAND_ID
+  LGINVOICE AS I
+ON
+  C.CUST_CODE = I.CUST_CODE
+JOIN
+  LGEMPLOYEE AS ES -- Alias for the employee who made the sale
+ON
+  I.EMPLOYEE_ID = ES.EMP_NUM
+JOIN
+  LGDEPARTMENT AS D
+ON
+  ES.DEPT_NUM = D.DEPT_NUM
+JOIN
+  LGEMPLOYEE AS EM -- Alias for the department manager
+ON
+  D.EMP_NUM = EM.EMP_NUM
 WHERE
-    B.BRAND_ID = 29 -- This line filters for the specific brand ID
-GROUP BY
-    B.BRAND_ID,
-    B.BRAND_NAME,
-    B.BRAND_TYPE;
+  C.CUST_LNAME = 'Hagan'
+  AND I.INV_DATE = '2021-05-18'
+  AND ES.EMP_FNAME = 'THURMAN' -- Added filter for the specific employee
+  AND ES.EMP_LNAME = 'WILKINSON'; -- Added filter for the specific employee
