0a2a47a51d645be524cfc816bacc719a9485bcf5
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 14:30:25 +0000

    Testing Sun Jun 08 2025 14:30:25 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -8,9 +8,9 @@ LEFT JOIN
     -- Join with WRITES and then BOOK to find books written by the author, specifically those with 'Programming' subject
     WRITES AS W ON A.AU_ID = W.AU_ID
 LEFT JOIN
-    BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM AND B.BOOK_SUBJECT = 'Programming'
+    BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM
 WHERE
     -- Filter for authors who had no matching 'Programming' book
-    B.BOOK_NUM IS NULL
+     B.BOOK_SUBJECT <> 'Programming'
 ORDER BY
     A.AU_LNAME ASC; -- Sort by author last name in ascending order
