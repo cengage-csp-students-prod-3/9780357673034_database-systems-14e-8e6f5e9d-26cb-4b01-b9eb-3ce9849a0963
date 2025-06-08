13f41e7a87b807c9859c56153ff0d27ae7ac365d
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 13:11:47 +0000

    Testing Sun Jun 08 2025 13:11:47 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,17 +1,20 @@
 SELECT
-    A.AU_ID,        -- Author ID
-    A.AU_FNAME,     -- Author First Name
-    A.AU_LNAME,     -- Author Last Name
-    B.BOOK_NUM,     -- Book Number
-    B.BOOK_TITLE    -- Book Title
+    B.BOOK_NUM,      -- Book Number
+    B.BOOK_TITLE,    -- Book Title
+    A.AU_LNAME,      -- Author Last Name
+    A.AU_FNAME,      -- Author First Name
+    P.PAT_ID,        -- Patron ID
+    P.PAT_LNAME,     -- Patron Last Name
+    P.PAT_TYPE       -- Patron Type
 FROM
-    AUTHOR AS A
+    BOOK AS B
 JOIN
-    WRITES AS W ON A.AU_ID = W.AU_ID -- Link Author to Written Books
+    PATRON AS P ON B.PAT_ID = P.PAT_ID -- Join BOOK with PATRON for current checkouts
 JOIN
-    BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM -- Link Written Books to Book details
+    WRITES AS W ON B.BOOK_NUM = W.BOOK_NUM -- Join BOOK with WRITES to link to authors
+JOIN
+    AUTHOR AS A ON W.AU_ID = A.AU_ID -- Join WRITES with AUTHOR for author details
 WHERE
-    B.BOOK_SUBJECT = 'Cloud' -- Filter for books in the 'Cloud' subject
+    B.PAT_ID IS NOT NULL -- Ensures only currently checked out books are included
 ORDER BY
-    B.BOOK_TITLE ASC, -- Sort by book title in ascending order
-    A.AU_LNAME ASC;   -- Then by author last name in ascending order
+    B.BOOK_TITLE ASC;    -- Sort results by book title
