07bb5e97bd84570a576547e5395d66e898d68659
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 11:55:28 +0000

    Testing Sun Jun 08 2025 11:55:28 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,4 +1,16 @@
 SELECT
-    SUM(BOOK_COST) AS `LIBRARY VALUE`
+    PAT_ID AS PATRON,
+    BOOK_NUM AS BOOK,
+    -- Calculate "Days Kept" as the difference between check-in and check-out dates.
+    -- If CHECK_IN_DATE is NULL (book not returned), DaysKept will also be NULL.
+    -- The specific function for date difference may vary slightly by database system:
+    -- SQL Server: DATEDIFF(day, CHECK_OUT_DATE, CHECK_IN_DATE)
+    -- MySQL: DATEDIFF(CHECK_IN_DATE, CHECK_OUT_DATE)
+    -- PostgreSQL/Oracle: CHECK_IN_DATE - CHECK_OUT_DATE (results in days for DATE types)
+       DATEDIFF(CHECK_IN_DATE, CHECK_IN_DATE) AS `DAYS KEPT`
 FROM
-    BOOK;
+    CHECKOUT
+ORDER BY
+    `DAYS KEPT` DESC, -- Sort by days kept in descending order
+    PATRON ASC,    -- Then by patron ID in ascending order
+     BOOK ASC;  -- Then by book number in ascending order
