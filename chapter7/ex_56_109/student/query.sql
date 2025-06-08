f96349332a27786dcce23e86b9b218c1b40c5453
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 14:46:43 +0000

    Testing Sun Jun 08 2025 14:46:43 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,17 +1,31 @@
+WITH AuthorTotalBooks AS (
+    -- First, calculate the total number of books written by each author, regardless of subject
+    SELECT
+        A.AU_ID,
+        COUNT(W.BOOK_NUM) AS TotalBooksByAuthor
+    FROM
+        AUTHOR AS A
+    JOIN
+        WRITES AS W ON A.AU_ID = W.AU_ID
+    GROUP BY
+        A.AU_ID
+)
 SELECT
     B.BOOK_NUM,         -- Book Number
     B.BOOK_TITLE,       -- Book Title
     B.BOOK_SUBJECT,     -- Book Subject
     A.AU_LNAME,         -- Author Last Name
-    COUNT(W.BOOK_NUM) OVER (PARTITION BY A.AU_ID) AS NumBooksByAuthor -- Counts all books written by each author
+    ATB.TotalBooksByAuthor AS `Num Books by Author` -- Use the pre-calculated total from the CTE
 FROM
     BOOK AS B
 JOIN
     WRITES AS W ON B.BOOK_NUM = W.BOOK_NUM -- Link books to the authors who wrote them
 JOIN
     AUTHOR AS A ON W.AU_ID = A.AU_ID       -- Link authors to their details
+JOIN
+    AuthorTotalBooks AS ATB ON A.AU_ID = ATB.AU_ID -- Join with the CTE to get total book count
 WHERE
-    B.BOOK_SUBJECT = 'Cloud' -- Filter for books specifically in the 'Cloud' subject
+    B.BOOK_SUBJECT = 'Cloud' -- Now, filter for books specifically in the 'Cloud' subject
 ORDER BY
     B.BOOK_TITLE ASC,   -- Sort by book title in ascending order
     A.AU_LNAME ASC;     -- Then by author last name in ascending order
