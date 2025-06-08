f94f368053e9a6883296acc283ff9d500045f8aa
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 13:16:32 +0000

    Testing Sun Jun 08 2025 13:16:32 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,14 +1,16 @@
 SELECT
     B.BOOK_NUM,
     B.BOOK_TITLE,
-    COUNT(C.CHECK_NUM) AS `TIMES CHECKED OUT` -- Counts non-null checkouts, resulting in 0 for books never checked out
+    COUNT(C.CHECK_NUM) AS `TIMES CHECKED OUT` -- Count the number of actual checkouts for each book
 FROM
     BOOK AS B
-LEFT JOIN
-    CHECKOUT AS C ON B.BOOK_NUM = C.BOOK_NUM -- Includes all books, even if they have no matching checkouts
+JOIN
+    CHECKOUT AS C ON B.BOOK_NUM = C.BOOK_NUM -- Use INNER JOIN to only include books that have been checked out
 GROUP BY
     B.BOOK_NUM,
     B.BOOK_TITLE
+HAVING
+    COUNT(C.CHECK_NUM) > 5 -- Filter for books checked out more than five times
 ORDER BY
     `TIMES CHECKED OUT` DESC, -- Sort by the number of times checked out in descending order
     B.BOOK_TITLE ASC;     -- Then by book title in ascending order
