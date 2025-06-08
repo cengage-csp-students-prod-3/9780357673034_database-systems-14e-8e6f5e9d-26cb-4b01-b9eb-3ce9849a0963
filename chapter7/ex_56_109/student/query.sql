939581e313ad753936f501cec72b969daf110d71
jcamdev247 <jcam.dev247@gmail.com>
2025-06-08 14:37:52 +0000

    Testing Sun Jun 08 2025 14:37:52 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,16 +1,18 @@
-SELECT DISTINCT
+SELECT
     A.AU_ID,       -- Author ID
     A.AU_FNAME,    -- Author First Name
     A.AU_LNAME     -- Author Last Name
 FROM
     AUTHOR AS A
-LEFT JOIN
-    -- Join with WRITES and then BOOK to find books written by the author, specifically those with 'Programming' subject
-    WRITES AS W ON A.AU_ID = W.AU_ID
-LEFT JOIN
-    BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM
-WHERE
-    -- Filter for authors who had no matching 'Programming' book
-     B.BOOK_SUBJECT NOT LIKE '%Programming'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM
+        WRITES AS W
+    JOIN
+        BOOK AS B ON W.BOOK_NUM = B.BOOK_NUM
+    WHERE
+        W.AU_ID = A.AU_ID          -- Link to the outer query's author
+        AND B.BOOK_SUBJECT = 'Programming' -- Filter for 'Programming' subject
+)
 ORDER BY
     A.AU_LNAME ASC; -- Sort by author last name in ascending order
