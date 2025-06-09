7426f4a72f61ab0a1cdd1326d849fc1f1e327e4f
jcamdev247 <jcam.dev247@gmail.com>
2025-06-09 14:00:40 +0000

    Testing Mon Jun 09 2025 14:00:40 GMT+0000 (Coordinated Universal Time)

--- chapter8/pa01/student/query.sql
+++ chapter8/pa01/student/query.sql
@@ -1,1 +1,5 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
+CREATE TABLE MAINTENANCE_TYPES (
+    MAINTENANCE_TYPE_ID CHAR(5) PRIMARY KEY NOT NULL,
+    MAINTENANCE_TYPE_DESCRIPTION VARCHAR(30) NOT NULL    
+);
