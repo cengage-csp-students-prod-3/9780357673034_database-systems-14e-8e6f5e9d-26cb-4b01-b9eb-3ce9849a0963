cd0767ad6def94a3f2c70a7242f89ede2b9a3615
jcamdev247 <jcam.dev247@gmail.com>
2025-06-09 15:13:55 +0000

    Testing Mon Jun 09 2025 15:13:55 GMT+0000 (Coordinated Universal Time)

--- chapter8/pa01/student/InstantRide.dump.sql
+++ chapter8/pa01/student/InstantRide.dump.sql
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `ACTIVE_DRIVERS`
+--
+
+DROP TABLE IF EXISTS `ACTIVE_DRIVERS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ACTIVE_DRIVERS` (
+  `DRIVER_ID` char(5) NOT NULL,
+  `DRIVER_FIRST_NAME` varchar(20) DEFAULT NULL,
+  `DRIVER_LAST_NAME` varchar(20) DEFAULT NULL,
+  `DRIVER_DRIVING_LICENSE_ID` varchar(10) DEFAULT NULL,
+  `DRIVER_DRIVING_LICENSE_CHECKED` tinyint(1) DEFAULT NULL,
+  `DRIVER_RATING` decimal(2,1) DEFAULT NULL,
+  PRIMARY KEY (`DRIVER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ACTIVE_DRIVERS`
+--
+
+LOCK TABLES `ACTIVE_DRIVERS` WRITE;
+/*!40000 ALTER TABLE `ACTIVE_DRIVERS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ACTIVE_DRIVERS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `CARS`
 --
 
@@ -37,7 +64,6 @@ CREATE TABLE `CARS` (
 
 LOCK TABLES `CARS` WRITE;
 /*!40000 ALTER TABLE `CARS` DISABLE KEYS */;
-INSERT INTO `CARS` VALUES ('1001','BB-542-AB','TOYOTA PRIUS',2021),('1002','BB-883-EE','TESLA MODEL 3',2022),('1003','BB-451-ZN','TOYOTA AURIS',2022),('1004','BB-189-MM','MERCEDES E200',2022);
 /*!40000 ALTER TABLE `CARS` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,7 +92,6 @@ CREATE TABLE `DRIVERS` (
 
 LOCK TABLES `DRIVERS` WRITE;
 /*!40000 ALTER TABLE `DRIVERS` DISABLE KEYS */;
-INSERT INTO `DRIVERS` VALUES ('2001','Willie','Butler','1874501','2022-09-12',1,4.4),('2002','Justin','Howard','1953853','2022-09-09',1,4.8),('2003','Anthony','Walker','1735487','2022-09-15',1,3.5),('2004','Ece','Yilmaz','1734747','2022-08-15',1,0.0);
 /*!40000 ALTER TABLE `DRIVERS` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,7 +173,6 @@ CREATE TABLE `TRAVELS` (
 
 LOCK TABLES `TRAVELS` WRITE;
 /*!40000 ALTER TABLE `TRAVELS` DISABLE KEYS */;
-INSERT INTO `TRAVELS` VALUES ('5001','2022-10-01 04:04:55','2022-10-01 04:14:19','9614 York Road','84 Church Lane',15.44,'2001','1003','3005',NULL),('5002','2022-10-01 05:57:33','2022-10-01 06:12:33','47 Church Street','68 High Street',20.56,'2001','1003','3006',NULL),('5003','2022-10-01 13:35:20','2022-10-01 13:45:10','2 Windsor Road','95 West Street',12.32,'2002','1001','3002',NULL),('5004','2022-10-02 08:44:48','2022-10-02 09:15:28','9060 Mill Lane','27 Main Road',30.49,'2003','1002','3001',0.13),('5005','2022-10-02 16:38:54','2022-10-02 16:48:10','2 Queensway','24 Mill Lane',11.15,'2001','1003','3007',NULL),('5006','2022-10-03 19:12:14','2022-10-03 19:23:45','50 Main Road','93 Broadway',14.61,'2003','1002','3007',0.10),('5007','2022-10-03 16:06:36','2022-10-03 16:08:56','39 Park Road','91 West Street',4.41,'2002','1004','3003',0.14),('5008','2022-10-03 17:17:12','2022-10-03 17:37:42','37 The Drive','17 Stanley Road',25.12,'2001','1003','3001',0.25),('5009','2022-10-03 21:16:48','2022-10-03 21:26:18','77 Mill Road','724 Springfield Road',13.55,'2001','1003','3005',NULL),('5010','2022-10-03 23:21:40','2022-10-03 23:39:10','16 Church Road','30 North Road',25.62,'2003','1002','3003',0.20);
 /*!40000 ALTER TABLE `TRAVELS` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -174,7 +198,6 @@ CREATE TABLE `USERS` (
 
 LOCK TABLES `USERS` WRITE;
 /*!40000 ALTER TABLE `USERS` DISABLE KEYS */;
-INSERT INTO `USERS` VALUES ('3001','Jack','Hill','j.hill@xmail.com'),('3002','Ryan','Collins','r.collins@xmail.com'),('3003','Nursin','Yilmaz','n.atak@gmail.com'),('3004','Sarah','Price','s.price@xmail.com'),('3005','Bobby','Griffin','b.griffin@xmail.com'),('3006','Randy','Clark','r.clark@xmail.com'),('3007','Jose','Thomas','j.thomas@xmail.com'),('3008','Nursin','Yilmaz','n.yilmaz@xmail.com');
 /*!40000 ALTER TABLE `USERS` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -191,4 +214,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-09 14:37:03
+-- Dump completed on 2025-06-09 15:13:50
