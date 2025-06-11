7a11a641d87c79dd1e7b0c651e12b3b84ec31371
jcamdev247 <jcam.dev247@gmail.com>
2025-06-11 07:38:37 +0000

    Testing Wed Jun 11 2025 07:38:37 GMT+0000 (Coordinated Universal Time)

--- chapter8/pa04/student/query.sql
+++ chapter8/pa04/student/query.sql
@@ -1,31 +1,10 @@
-USE InstantRide;
-
--- Drop the procedure if it already exists to allow for recreation
-DROP PROCEDURE IF EXISTS VATCalculator;
-
--- Change the delimiter to allow for multiple statements within the procedure definition
-DELIMITER $$
-
+DELIMITER //
 CREATE PROCEDURE VATCalculator()
 BEGIN
-    -- Select the travel ID and calculate the VAT amount
     SELECT
-        T.TRAVEL_ID,
-        -- Calculate the effective price after applying the discount.
-        -- COALESCE handles NULL discounts by treating them as 0.
-        ROUND(
-            (T.TRAVEL_PRICE * (1 - COALESCE(T.TRAVEL_DISCOUNT, 0))) * 0.08,
-            2
-        ) AS Calculated_VAT_Amount
+        TRAVEL_ID,
+        ROUND(TRAVEL_PRICE * (1 - COALESCE(TRAVEL_DISCOUNT, 0)) * 0.08, 2) AS VAT_AMOUNT
     FROM
-        TRAVELS AS T;
-END$$
-
--- Reset the delimiter back to the standard semicolon
+        TRAVELS;
+END //
 DELIMITER ;
-
--- Execute the VATCalculator procedure to get the results
-CALL VATCalculator();
-
-SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'InstantRide' AND TABLE_NAME = 'TRAVELS';
-
