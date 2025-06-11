21494ad3aa26b9292a7fcbf55744d01ebe890427
jcamdev247 <jcam.dev247@gmail.com>
2025-06-11 06:59:27 +0000

    Testing Wed Jun 11 2025 06:59:27 GMT+0000 (Coordinated Universal Time)

--- chapter8/pa04/student/InstantRide.dump.sql
+++ chapter8/pa04/student/InstantRide.dump.sql
@@ -129,6 +129,45 @@ DELIMITER ;
 --
 -- Dumping routines for database 'InstantRide'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `EmailList` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `EmailList`(OUT result TEXT)
+BEGIN
+DECLARE done INT DEFAULT FALSE;
+DECLARE email_var VARCHAR(100);
+DECLARE email_list TEXT DEFAULT '';
+DECLARE email_cursor CURSOR FOR
+SELECT USER_EMAIL FROM USERS ORDER BY USER_ID;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+OPEN email_cursor;
+email_loop: LOOP
+FETCH email_cursor INTO email_var;
+IF done THEN
+LEAVE email_loop;
+END IF;
+IF email_list = '' THEN
+SET email_list = email_var;
+ELSE
+SET email_list = CONCAT(email_list, ';', email_var);
+END IF;
+END LOOP;
+CLOSE email_cursor;
+
+SET result = CONCAT(';', email_list);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -139,4 +178,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-11  6:57:13
+-- Dump completed on 2025-06-11  6:59:25
--- chapter8/pa04/student/query.sql
+++ chapter8/pa04/student/query.sql
@@ -1,18 +1,29 @@
 USE InstantRide;
+DROP PROCEDURE IF EXISTS EmailList;
 DELIMITER $$
-CREATE PROCEDURE VATCalculator()
+CREATE PROCEDURE EmailList(OUT result TEXT)
 BEGIN
-SELECT
-TRAVEL_ID,
-ROUND(
-CASE
-WHEN TRAVEL_DISCOUNT IS NULL THEN TRAVEL_PRICE * 0.08
-ELSE (TRAVEL_PRICE (1 - TRAVEL_DISCOUNT)) 0.08
-END
-, 2) AS VAT
-FROM
-TRAVELS
-ORDER BY
-TRAVEL_ID;
-END $$
+DECLARE done INT DEFAULT FALSE;
+DECLARE email_var VARCHAR(100);
+DECLARE email_list TEXT DEFAULT '';
+DECLARE email_cursor CURSOR FOR
+SELECT USER_EMAIL FROM USERS ORDER BY USER_ID;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+OPEN email_cursor;
+email_loop: LOOP
+FETCH email_cursor INTO email_var;
+IF done THEN
+LEAVE email_loop;
+END IF;
+IF email_list = '' THEN
+SET email_list = email_var;
+ELSE
+SET email_list = CONCAT(email_list, ';', email_var);
+END IF;
+END LOOP;
+CLOSE email_cursor;
+
+SET result = CONCAT(';', email_list);
+END$$
 DELIMITER ;
+CALL EmailList();
