9902b17ea79f42ebe0c41adc6a735c91ff3f13a1
jcamdev247 <jcam.dev247@gmail.com>
2025-06-11 07:34:15 +0000

    Testing Wed Jun 11 2025 07:34:15 GMT+0000 (Coordinated Universal Time)

--- chapter8/pa04/student/InstantRide.dump.sql
+++ chapter8/pa04/student/InstantRide.dump.sql
@@ -148,6 +148,32 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `VATCalculator` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `VATCalculator`()
+BEGIN
+         SELECT
+        T.TRAVEL_ID,
+                          ROUND(
+            (T.TRAVEL_PRICE * (1 - COALESCE(T.TRAVEL_DISCOUNT, 0))) * 0.08,
+            2
+        ) AS Calculated_VAT_Amount
+    FROM
+        TRAVELS AS T;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -158,4 +184,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-11  7:26:04
+-- Dump completed on 2025-06-11  7:34:09
--- chapter8/pa04/student/query.sql
+++ chapter8/pa04/student/query.sql
@@ -1,10 +1,28 @@
 USE InstantRide;
-DROP PROCEDURE IF EXISTS EmailList;
+
+-- Drop the procedure if it already exists to allow for recreation
+DROP PROCEDURE IF EXISTS VATCalculator;
+
+-- Change the delimiter to allow for multiple statements within the procedure definition
 DELIMITER $$
-CREATE PROCEDURE EmailList(OUT result TEXT)
+
+CREATE PROCEDURE VATCalculator()
 BEGIN
-    SELECT GROUP_CONCAT(USER_EMAIL ORDER BY USER_ID SEPARATOR ';') INTO result FROM USERS;
+    -- Select the travel ID and calculate the VAT amount
+    SELECT
+        T.TRAVEL_ID,
+        -- Calculate the effective price after applying the discount.
+        -- COALESCE handles NULL discounts by treating them as 0.
+        ROUND(
+            (T.TRAVEL_PRICE * (1 - COALESCE(T.TRAVEL_DISCOUNT, 0))) * 0.08,
+            2
+        ) AS Calculated_VAT_Amount
+    FROM
+        TRAVELS AS T;
 END$$
+
+-- Reset the delimiter back to the standard semicolon
 DELIMITER ;
-CALL EmailList(@email_list_result);
-SELECT @email_list_result;
+
+-- Execute the VATCalculator procedure to get the results
+CALL VATCalculator();
