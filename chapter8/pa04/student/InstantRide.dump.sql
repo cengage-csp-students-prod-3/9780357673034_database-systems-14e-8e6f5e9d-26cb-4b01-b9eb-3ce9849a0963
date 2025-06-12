0538e55873510cad3f95e79c53dcaeec670eba32
jcamdev247 <jcam.dev247@gmail.com>
2025-06-12 01:29:38 +0000

    Testing Thu Jun 12 2025 01:29:38 GMT+0000 (Coordinated Universal Time)

--- chapter8/pa04/student/InstantRide.dump.sql
+++ chapter8/pa04/student/InstantRide.dump.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `CARS`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `CARS` (
-  `CAR_ID` char(5) NOT NULL,
+  `CAR_ID` int NOT NULL,
   `CAR_PLATE` varchar(10) NOT NULL,
   `CAR_MODEL` varchar(20) NOT NULL,
   `CAR_YEAR` year NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE `CARS` (
 
 LOCK TABLES `CARS` WRITE;
 /*!40000 ALTER TABLE `CARS` DISABLE KEYS */;
-INSERT INTO `CARS` VALUES ('1001','BB-542-AB','TOYOTA PRIUS',2021),('1002','BB-883-EE','TESLA MODEL 3',2022),('1003','BB-451-ZN','TOYOTA AURIS',2022),('1004','BB-189-MM','MERCEDES E200',2022);
+INSERT INTO `CARS` VALUES (1001,'BB-542-AB','TOYOTA PRIUS',2021),(1002,'BB-883-EE','TESLA MODEL 3',2022),(1003,'BB-451-ZN','TOYOTA AURIS',2022),(1004,'BB-189-MM','MERCEDES E200',2022);
 /*!40000 ALTER TABLE `CARS` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,7 +49,7 @@ DROP TABLE IF EXISTS `DRIVERS`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `DRIVERS` (
-  `DRIVER_ID` char(5) NOT NULL,
+  `DRIVER_ID` int NOT NULL,
   `DRIVER_FIRST_NAME` varchar(20) NOT NULL,
   `DRIVER_LAST_NAME` varchar(20) NOT NULL,
   `DRIVER_DRIVING_LICENSE_ID` varchar(10) NOT NULL,
@@ -66,8 +66,45 @@ CREATE TABLE `DRIVERS` (
 
 LOCK TABLES `DRIVERS` WRITE;
 /*!40000 ALTER TABLE `DRIVERS` DISABLE KEYS */;
-INSERT INTO `DRIVERS` VALUES ('2001','Willie','Butler','1874501','2022-09-12',1,4.4),('2002','Justin','Howard','1953853','2022-09-09',1,4.8),('2003','Anthony','Walker','1735487','2022-09-15',1,3.5),('2004','Ece','Yilmaz','1734747','2022-08-15',1,0.0);
+INSERT INTO `DRIVERS` VALUES (2001,'Willie','Butler','1874501','2022-09-12',1,4.4),(2002,'Justin','Howard','1953853','2022-09-09',1,4.8),(2003,'Anthony','Walker','1735487','2022-09-15',1,3.5),(2004,'Ece','Yilmaz','1734747','2022-08-15',1,0.0);
 /*!40000 ALTER TABLE `DRIVERS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `TRAVELS`
+--
+
+DROP TABLE IF EXISTS `TRAVELS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `TRAVELS` (
+  `TRAVEL_ID` char(10) NOT NULL,
+  `TRAVEL_START_TIME` datetime NOT NULL,
+  `TRAVEL_END_TIME` datetime NOT NULL,
+  `TRAVEL_START_LOCATION` char(30) NOT NULL,
+  `TRAVEL_END_LOCATION` char(30) NOT NULL,
+  `TRAVEL_PRICE` decimal(5,2) NOT NULL,
+  `DRIVER_ID` int NOT NULL,
+  `CAR_ID` int NOT NULL,
+  `USER_ID` int NOT NULL,
+  `TRAVEL_DISCOUNT` decimal(3,2) DEFAULT NULL,
+  PRIMARY KEY (`TRAVEL_ID`),
+  KEY `DRIVER_ID` (`DRIVER_ID`),
+  KEY `CAR_ID` (`CAR_ID`),
+  KEY `USER_ID` (`USER_ID`),
+  CONSTRAINT `TRAVELS_ibfk_1` FOREIGN KEY (`DRIVER_ID`) REFERENCES `DRIVERS` (`DRIVER_ID`),
+  CONSTRAINT `TRAVELS_ibfk_2` FOREIGN KEY (`CAR_ID`) REFERENCES `CARS` (`CAR_ID`),
+  CONSTRAINT `TRAVELS_ibfk_3` FOREIGN KEY (`USER_ID`) REFERENCES `USERS` (`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `TRAVELS`
+--
+
+LOCK TABLES `TRAVELS` WRITE;
+/*!40000 ALTER TABLE `TRAVELS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TRAVELS` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -78,7 +115,7 @@ DROP TABLE IF EXISTS `USERS`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `USERS` (
-  `USER_ID` char(5) NOT NULL,
+  `USER_ID` int NOT NULL,
   `USER_FIRST_NAME` varchar(20) NOT NULL,
   `USER_LAST_NAME` varchar(20) NOT NULL,
   `USER_EMAIL` varchar(100) NOT NULL,
@@ -92,88 +129,13 @@ CREATE TABLE `USERS` (
 
 LOCK TABLES `USERS` WRITE;
 /*!40000 ALTER TABLE `USERS` DISABLE KEYS */;
-INSERT INTO `USERS` VALUES ('3001','Jack','Hill','j.hill@xmail.com'),('3002','Ryan','Collins','r.collins@xmail.com'),('3003','Nursin','Yilmaz','n.atak@gmail.com'),('3004','Sarah','Price','s.price@xmail.com'),('3005','Bobby','Griffin','b.griffin@xmail.com'),('3006','Randy','Clark','r.clark@xmail.com'),('3007','Jose','Thomas','j.thomas@xmail.com'),('3008','Nursin','Yilmaz','n.yilmaz@xmail.com');
+INSERT INTO `USERS` VALUES (3001,'Jack','Hill','j.hill@xmail.com'),(3002,'Ryan','Collins','r.collins@xmail.com'),(3003,'Nursin','Yilmaz','n.atak@gmail.com'),(3004,'Sarah','Price','s.price@xmail.com'),(3005,'Bobby','Griffin','b.griffin@xmail.com'),(3006,'Randy','Clark','r.clark@xmail.com'),(3007,'Jose','Thomas','j.thomas@xmail.com'),(3008,'Nursin','Yilmaz','n.yilmaz@xmail.com');
 /*!40000 ALTER TABLE `USERS` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `email_insert` BEFORE INSERT ON `USERS` FOR EACH ROW SET NEW . USER_EMAIL = LOWER(NEW.USER_EMAIL) */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `email_update` BEFORE UPDATE ON `USERS` FOR EACH ROW SET NEW . USER_EMAIL = LOWER(NEW.USER_EMAIL) */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Dumping routines for database 'InstantRide'
 --
-/*!50003 DROP PROCEDURE IF EXISTS `EmailList` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `EmailList`(OUT result TEXT)
-BEGIN
-    SELECT GROUP_CONCAT(USER_EMAIL ORDER BY USER_ID SEPARATOR ';') INTO result FROM USERS;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `VATCalculator` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `VATCalculator`()
-BEGIN
-         SELECT
-        T.TRAVEL_ID,
-                          ROUND(
-            (T.TRAVEL_PRICE * (1 - COALESCE(T.TRAVEL_DISCOUNT, 0))) * 0.08,
-            2
-        ) AS Calculated_VAT_Amount
-    FROM
-        TRAVELS AS T;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -184,4 +146,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-11  7:38:35
+-- Dump completed on 2025-06-12  1:29:32
