e8a648239f77732ec23b74bd530b057a878f0b26
jcamdev247 <jcam.dev247@gmail.com>
2025-06-12 07:37:24 -0400

    Update query.sql
    *

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,10 +1,10 @@
 SELECT
     BOOK_SUBJECT,
-    COUNT(*) AS `BOOKS IN SUBJECT` -- Changed alias to remove spaces
+    COUNT(*) AS `BOOKS IN SUBJECT` 
 FROM
     BOOK
 GROUP BY
     BOOK_SUBJECT
 ORDER BY
-    `BOOKS IN SUBJECT` DESC,    -- Reference the alias directly (unquoted)
+    `BOOKS IN SUBJECT` DESC,   
     BOOK_SUBJECT ASC;
