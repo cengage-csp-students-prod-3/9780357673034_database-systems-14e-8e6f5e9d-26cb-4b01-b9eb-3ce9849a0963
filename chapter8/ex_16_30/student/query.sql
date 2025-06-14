89ee576ccdd46684cf8864f955f8b1fda2595ea5
jcamdev247 <jcam.dev247@gmail.com>
2025-06-14 03:19:08 +0000

    Testing Sat Jun 14 2025 03:19:08 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_16_30/student/query.sql
+++ chapter8/ex_16_30/student/query.sql
@@ -1,1 +1,13 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
+DROP TABLE IF EXISTS CUSTOMER;
+
+CREATE TABLE CUSTOMER(
+ CUST_NUM INT,
+ CUST_LNAME VARCHAR(30),
+ CUST_FNAME VARCHAR(30),
+ CUST_BALANCE DECIMAL(8,2),
+ PRIMARY KEY (CUST_NUM)   
+);
+
+SELECT *
+FROM CUSTOMER;
