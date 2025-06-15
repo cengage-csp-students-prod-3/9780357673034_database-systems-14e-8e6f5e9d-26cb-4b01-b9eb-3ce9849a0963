a850a5a2bfc3e3d934db89896c4e6068df67c830
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 21:05:14 +0000

    Testing Sun Jun 15 2025 21:05:14 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_35_46/student/query.sql
+++ chapter8/ex_35_46/student/query.sql
@@ -1,9 +1,1 @@
--- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-ALTER TABLE CHARTER
-ADD  CHAR_WAIT_CHG decimal(6,2),
-ADD  CHAR_FLT_CHG_HR decimal(6,2),
-ADD  CHAR_FLT_CHG decimal(10,2),
-ADD  CHAR_TAX_CHG decimal(6,2),
-ADD  CHAR_TOT_CHG decimal(10,2),
-ADD  CHAR_PYMT decimal(10,2),
-ADD  CHAR_BALANCE decimal(10,2);
+DESCRIBE CHARTER
