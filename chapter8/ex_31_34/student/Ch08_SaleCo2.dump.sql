1d40df95f94a01782b655f08db73ddb0e80b35cb
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 04:17:01 +0000

    Testing Sun Jun 15 2025 04:17:01 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_31_34/student/Ch08_SaleCo2.dump.sql
+++ chapter8/ex_31_34/student/Ch08_SaleCo2.dump.sql
@@ -193,6 +193,40 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'Ch08_SaleCo2'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `prc_cus_balance_update` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `prc_cus_balance_update`(
+    IN p_inv_number INT
+)
+BEGIN
+    DECLARE v_tot_inv DECIMAL(10,2);
+    DECLARE v_cus_code INT;
+
+         SELECT INV_TOTAL, CUS_CODE
+    INTO v_tot_inv, v_cus_code
+    FROM INVOICE
+    WHERE INV_NUMBER = p_inv_number;
+
+         IF v_cus_code IS NOT NULL THEN
+                 UPDATE CUSTOMER
+        SET CUS_BALANCE = CUS_BALANCE + v_tot_inv
+        WHERE CUS_CODE = v_cus_code;
+    END IF;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `prc_inv_amounts` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -248,4 +282,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-15  4:16:17
+-- Dump completed on 2025-06-15  4:16:58
--- chapter8/ex_31_34/student/query.sql
+++ chapter8/ex_31_34/student/query.sql
@@ -1,14 +1,26 @@
 DELIMITER //
 
-CREATE TRIGGER trg_line_prod
-AFTER INSERT ON LINE
-FOR EACH ROW
+CREATE PROCEDURE prc_cus_balance_update (
+    IN p_inv_number INT
+)
 BEGIN
-    -- Decrease the product's quantity on hand (P_QOH)
-    -- by the number of units sold (LINE_UNITS) in the new line item.
-    UPDATE PRODUCT
-    SET P_QOH = P_QOH - NEW.LINE_UNITS
-    WHERE P_CODE = NEW.P_CODE;
+    DECLARE v_tot_inv DECIMAL(10,2);
+    DECLARE v_cus_code INT;
+
+    -- Retrieve the invoice total and customer code for the given invoice number
+    SELECT INV_TOTAL, CUS_CODE
+    INTO v_tot_inv, v_cus_code
+    FROM INVOICE
+    WHERE INV_NUMBER = p_inv_number;
+
+    -- Check if an invoice was found
+    IF v_cus_code IS NOT NULL THEN
+        -- Update the customer's balance by adding the invoice total
+        UPDATE CUSTOMER
+        SET CUS_BALANCE = CUS_BALANCE + v_tot_inv
+        WHERE CUS_CODE = v_cus_code;
+    END IF;
+
 END;
 //
 
