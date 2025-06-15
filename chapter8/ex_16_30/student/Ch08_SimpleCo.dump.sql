db126fed6d00540876d9b3247bc438756e2abf9f
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 04:00:15 +0000

    Testing Sun Jun 15 2025 04:00:15 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_16_30/student/Ch08_SimpleCo.dump.sql
+++ chapter8/ex_16_30/student/Ch08_SimpleCo.dump.sql
@@ -88,6 +88,25 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `trg_updatecustbalance2` AFTER DELETE ON `INVOICE` FOR EACH ROW BEGIN
+    UPDATE CUSTOMER
+    SET CUST_BALANCE = CUST_BALANCE - OLD.INV_AMOUNT
+    WHERE CUST_NUM = OLD.CUST_NUM;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Dumping routines for database 'Ch08_SimpleCo'
@@ -150,4 +169,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-15  3:57:27
+-- Dump completed on 2025-06-15  4:00:13
