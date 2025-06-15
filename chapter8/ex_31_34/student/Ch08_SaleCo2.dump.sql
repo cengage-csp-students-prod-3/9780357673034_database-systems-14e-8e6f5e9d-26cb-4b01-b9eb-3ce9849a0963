35ab4b0f9051a9a55425c644f80eb9b1b2578c5c
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 04:16:18 +0000

    Testing Sun Jun 15 2025 04:16:18 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_31_34/student/Ch08_SaleCo2.dump.sql
+++ chapter8/ex_31_34/student/Ch08_SaleCo2.dump.sql
@@ -113,6 +113,25 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `trg_line_prod` AFTER INSERT ON `LINE` FOR EACH ROW BEGIN
+              UPDATE PRODUCT
+    SET P_QOH = P_QOH - NEW.LINE_UNITS
+    WHERE P_CODE = NEW.P_CODE;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `PRODUCT`
@@ -229,4 +248,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-15  4:15:11
+-- Dump completed on 2025-06-15  4:16:17
--- chapter8/ex_31_34/student/query.sql
+++ chapter8/ex_31_34/student/query.sql
@@ -1,40 +1,14 @@
 DELIMITER //
 
-CREATE PROCEDURE prc_inv_amounts (
-    IN p_inv_number INT
-)
+CREATE TRIGGER trg_line_prod
+AFTER INSERT ON LINE
+FOR EACH ROW
 BEGIN
-    DECLARE v_inv_subtotal DECIMAL(10,2);
-    DECLARE v_inv_tax DECIMAL(10,2);
-    DECLARE v_inv_total DECIMAL(10,2);
-    DECLARE v_tax_rate DECIMAL(3,2) DEFAULT 0.08; -- 8 percent tax rate
-
-    -- Calculate INV_SUBTOTAL by summing LINE_TOTAL for the given invoice
-    SELECT SUM(LINE_TOTAL)
-    INTO v_inv_subtotal
-    FROM LINE
-    WHERE INV_NUMBER = p_inv_number;
-
-    -- If no lines exist for the invoice, set subtotal to 0
-    IF v_inv_subtotal IS NULL THEN
-        SET v_inv_subtotal = 0;
-    END IF;
-
-    -- Calculate INV_TAX
-    SET v_inv_tax = v_inv_subtotal * v_tax_rate;
-
-    -- Calculate INV_TOTAL
-    SET v_inv_total = v_inv_subtotal + v_inv_tax;
-
-    -- Update the INVOICE table with the calculated values
-    UPDATE INVOICE
-    SET
-        INV_SUBTOTAL = v_inv_subtotal,
-        INV_TAX = v_inv_tax,
-        INV_TOTAL = v_inv_total
-    WHERE
-        INV_NUMBER = p_inv_number;
-
+    -- Decrease the product's quantity on hand (P_QOH)
+    -- by the number of units sold (LINE_UNITS) in the new line item.
+    UPDATE PRODUCT
+    SET P_QOH = P_QOH - NEW.LINE_UNITS
+    WHERE P_CODE = NEW.P_CODE;
 END;
 //
 
