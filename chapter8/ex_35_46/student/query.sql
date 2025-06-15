abe88afa36dd8c20cac8035d3f639eaab5bab79c
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 21:04:02 +0000

    Testing Sun Jun 15 2025 21:04:02 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_35_46/student/query.sql
+++ chapter8/ex_35_46/student/query.sql
@@ -1,9 +1,9 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-UPDATE MODEL
-SET  MOD_WAIT_CHG = 50.00
-WHERE MOD_CODE = 'PA23-250';
-
-
-UPDATE MODEL
-SET  MOD_WAIT_CHG = 75.00
-WHERE MOD_CODE = 'PA31-350';
+ALTER TABLE CHARTER
+ADD  CHAR_WAIT_CHG decimal(6,2),
+ADD  CHAR_FLT_CHG_HR decimal(6,2),
+ADD  CHAR_FLT_CHG decimal(10,2),
+ADD  CHAR_TAX_CHG decimal(6,2),
+ADD  CHAR_TOT_CHG decimal(10,2),
+ADD  CHAR_PYMT decimal(10,2),
+ADD  CHAR_BALANCE decimal(10,2);
