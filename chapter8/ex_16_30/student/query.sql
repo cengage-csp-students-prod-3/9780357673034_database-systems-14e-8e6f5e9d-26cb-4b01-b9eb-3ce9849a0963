e2570017d8c6cd9df41e3493a6327b4f15ef6d54
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 03:37:32 +0000

    Testing Sun Jun 15 2025 03:37:32 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_16_30/student/query.sql
+++ chapter8/ex_16_30/student/query.sql
@@ -1,4 +1,13 @@
--- Create the trigger named trg_updatecustbalance
-UPDATE CUSTOMER
-SET CUST_BALANCE = 1066.32
-WHERE CUST_NUM = 1001;
+DELIMITER $$
+CREATE PROCEDURE prc_cust_add(
+    IN p_cust_lname VARCHAR(30),
+    IN p_cust_fname VARCHAR(30),
+    IN p_cust_balance DECIMAL(10, 2)
+)
+BEGIN
+    -- Inserts a new customer record into the CUSTOMER table.
+    -- CUST_NUM is auto-incremented.
+    INSERT INTO CUSTOMER (CUST_LNAME, CUST_FNAME, CUST_BALANCE)
+    VALUES (p_cust_lname, p_cust_fname, p_cust_balance);
+END$$
+DELIMITER ;
