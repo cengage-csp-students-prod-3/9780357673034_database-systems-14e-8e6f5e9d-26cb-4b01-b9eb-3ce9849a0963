5e8b9fa091b422be41bcf8161e75af0b797ad824
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 03:36:24 +0000

    Testing Sun Jun 15 2025 03:36:24 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_16_30/student/query.sql
+++ chapter8/ex_16_30/student/query.sql
@@ -1,15 +1,4 @@
 -- Create the trigger named trg_updatecustbalance
-DELIMITER $$
-CREATE TRIGGER trg_updatecustbalance
-AFTER INSERT ON INVOICE
-FOR EACH ROW
-BEGIN
-    -- IMPORTANT: Ensure 'Ch08_SimpleCo' database is selected (e.g., via 'USE Ch08_SimpleCo;' at the top)
-    -- and that the CUSTOMER and INVOICE tables exist in the selected database.
-    -- This trigger updates the CUST_BALANCE in the CUSTOMER table.
-    -- It adds the new invoice's INV_AMOUNT to the customer's balance.
-    UPDATE CUSTOMER
-    SET CUST_BALANCE = CUST_BALANCE + NEW.INV_AMOUNT
-    WHERE CUST_NUM = NEW.CUST_NUM;
-END$$
-DELIMITER ;
+UPDATE CUSTOMER
+SET CUST_BALANCE = 1066.32
+WHERE CUST_NUM = 1001;
