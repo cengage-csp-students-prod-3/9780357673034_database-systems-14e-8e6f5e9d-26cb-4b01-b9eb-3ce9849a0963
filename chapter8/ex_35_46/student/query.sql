0622d44748e71bb640d2d4ffc6a33c5467c729e0
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 20:41:53 +0000

    Testing Sun Jun 15 2025 20:41:53 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_35_46/student/query.sql
+++ chapter8/ex_35_46/student/query.sql
@@ -1,1 +1,12 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
+INSERT INTO MODEL (MOD_WAIT_CHG)
+VALUES(100.00)
+WHERE MOD_CODE = 'C-90A';
+
+INSERT INTO MODEL (MOD_WAIT_CHG)
+VALUES(50.00)
+WHERE MOD_CODE = 'PA23-250';
+
+INSERT INTO MODEL (MOD_WAIT_CHG)
+VALUES(75.00)
+WHERE MOD_CODE = 'PA23-350';
