a0a96546c893292699915d03e0b2f6a7fb62f8bf
jcamdev247 <jcam.dev247@gmail.com>
2025-06-15 03:56:33 +0000

    Testing Sun Jun 15 2025 03:56:33 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_16_30/student/query.sql
+++ chapter8/ex_16_30/student/query.sql
@@ -1,20 +1,6 @@
-DROP PROCEDURE IF EXISTS prc_invoice_add;
+TRUNCATE TABLE INVOICE;
 
--- Create a procedure named prc_invoice_add to add a new invoice record
-DELIMITER $$
-CREATE PROCEDURE prc_invoice_add(
-    IN p_cust_num INT,
-    IN p_inv_date DATE,
-    IN p_inv_amount DECIMAL(10, 2)
-)
-BEGIN
-    -- Inserts a new invoice record into the INVOICE table.
-    -- INV_NUM is auto-incremented.
-    INSERT INTO INVOICE (CUST_NUM, INV_DATE, INV_AMOUNT)
-    VALUES (p_cust_num, p_inv_date, p_inv_amount);
-END$$
-DELIMITER ;
+INSERT INTO INVOICE (INV_NUM, CUST_NUM, INV_DATE, INV_AMOUNT)
+VALUES (8006, 1000, '2022/04/30', 301.72 );
 
--- Test the new prc_invoice_add procedure by adding a new invoice record
--- This will automatically trigger trg_updatecustbalance and update customer 1000's balance
-CALL prc_invoice_add(1000, '2022-04-30', 301.72);
+SELECT * FROM INVOICE;  
